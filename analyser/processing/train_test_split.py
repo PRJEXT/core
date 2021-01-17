9f3406fcc8ae460a8d4f5fcb5fbb064b46ea2cf2
Hugo Isaac <hugo_iar@live.com>
2021-01-17 20:27:34 -0300

    Update train_test_split and add docs

--- analyser/processing/train_test_split.py
+++ analyser/processing/train_test_split.py
@@ -3,10 +3,15 @@ from analyser.preprocessing.bag_of_words import generate_bag_of_words
 
 
 def split(data_frame, sentiment_column, preprocessed_column):
+    '''Realiza a separação dos dados de treino e de teste a partir das colunas de reviews e sentimentos do data frame.'''
+    # X representa o que será utilizado para a predição (reviews pré-processadas/variável independente)
     X = generate_bag_of_words(data_frame, preprocessed_column)
+    # y representa o que queremos prever (sentimentos/variável dependente)
     y = data_frame[sentiment_column]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+    seed = 1234 # TODO: mudar para um lugar central (compartilhado com o módulo de importação de dados)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=seed)
 
     sets = {
         'X_train': X_train,
