eb6ddd9f089e42df498a585d490a152d9887564b
Hugo Isaac <hugo_iar@live.com>
2021-01-18 20:29:41 -0300

    Add average parameter to evaluate_model

--- analyser/evaluation/evaluate_model.py
+++ analyser/evaluation/evaluate_model.py
@@ -3,6 +3,8 @@ from sklearn.metrics import f1_score
 
 def evaluate_model(y_test, prediction):
     '''Avalia o modelo utilizando a métrica F1 Score.'''
-    score = f1_score(y_test, prediction)
+    # O parâmetro average é necessário por termos multiclass/multilabel targets,
+    # ele altera como as métricas são calculadas
+    score = f1_score(y_test, prediction, average='micro')
     
     return score
