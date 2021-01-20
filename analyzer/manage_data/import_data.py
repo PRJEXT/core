390031111b2b764b5d516c1b2b7f61ef8b463b52
Hugo Isaac <hugo_iar@live.com>
2021-01-20 20:23:13 -0300

    Update import_data module

--- analyzer/manage_data/import_data.py
+++ analyzer/manage_data/import_data.py
@@ -1,17 +1,14 @@
 import pandas as pd
 
 
-def import_data(path=None, size='full'):
-    '''Importa o dataset e retorna um dataframe.'''
-    if path:
-        path = path
-    else:
-        path = 'data/train.csv'
+def import_data(file_name, sample='full'):
+    '''Importa um arquivo csv da pasta data e retorna um dataframe.'''
+    path = f'data/{file_name}.csv'
     
     data_frame = pd.read_csv(path)
-    seed = 1234
 
-    if size == 'full':
+    if sample == 'full':
         return data_frame
-    elif size == 'small':
-        return data_frame.sample(1000, random_state=seed)
+    else:
+        seed = 1234
+        return data_frame.sample(sample, random_state=seed)
