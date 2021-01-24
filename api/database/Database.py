44c2b0e89b95b31c1558e9c884549cf7cb0be49a
Hugo Isaac <hugo_iar@live.com>
2021-01-23 23:25:30 -0300

    Refact ReviewService to Database

--- api/database/Database.py
+++ api/database/Database.py
@@ -1,8 +1,17 @@
 from os import getenv
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 
 class Database:
     def __init__(self):
         self.client = MongoClient(getenv('MONGO_URI'))
+        self.COLLECTION = 'reviews'
         self.db = self.client["Cluster0"]
+        self.collection = self.db[self.COLLECTION]
+    
+    def update(self, id, sentiment):
+        query = { "_id": ObjectId(id) }
+        props = { "$set": { "sentiment": sentiment, 'reviewed': True } }
+        updated = self.collection.update_one(query, props)
+        print(updated)
--- api/service/ListenerService.py
+++ api/service/ListenerService.py
@@ -3,7 +3,7 @@ import json
 from os import getenv
 
 from service.SentAnalysis import SentAnalysis
-from service.ReviewService import ReviewService
+from database.Database import Database
 
 
 class ListenerService:
@@ -11,7 +11,7 @@ class ListenerService:
         self.CHANNEL_NAME = getenv('REDIS_CHANNEL')
         # self.svc = SentAnalysis()
         self.redis = redis.Redis()
-        self.reviewService = ReviewService()
+        self.database = Database()
 
         self.pubsub = self.redis.pubsub()
         self.pubsub.psubscribe(self.CHANNEL_NAME)
@@ -24,5 +24,5 @@ class ListenerService:
                 id = data["id"]
                 review = data["review"]
                 print(id, review)
-                self.reviewService.update(id, review)
+                self.database.update(id, review)
                 print("Is the right channel", message["data"])
