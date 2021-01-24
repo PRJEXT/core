719af09d891a16a7c6eddaa5a1cc84b8cbf2bf4c
Hugo Isaac <hugo_iar@live.com>
2021-01-24 11:25:13 -0300

    Add write_concern to Database

--- api/database/Database.py
+++ api/database/Database.py
@@ -5,13 +5,11 @@ from bson.objectid import ObjectId
 
 class Database:
     def __init__(self):
-        self.client = MongoClient(getenv('MONGO_URI'))
-        self.COLLECTION = 'reviews'
+        self.client = MongoClient(getenv('MONGO_URI'), w=1)
         self.db = self.client["Cluster0"]
-        self.collection = self.db[self.COLLECTION]
+        self.collection = self.db['reviews']
     
     def update(self, id, sentiment):
         query = { "_id": ObjectId(id) }
         props = { "$set": { "sentiment": sentiment, 'reviewed': True } }
-        updated = self.collection.update_one(query, props)
-        print(updated)
+        self.collection.update_one(query, props)
